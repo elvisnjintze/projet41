6f920bc3b6246eecb72bc8c319fd755b2890a934
elvisnjintze <enjintze@gmail.com>
2023-06-12 18:04:32 +0100

    debut de developpement

---
+++ jeu.py
@@ -0,0 +1,9 @@
+"""
+ce fichier contient les fonctionalité sur les jioueurs
+"""
+
+class Joueur():
+    """"classe représentant un joueur"""
+    def __init__(self,name):
+        self.name = name
+        
